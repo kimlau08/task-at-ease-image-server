f8d21b63c9158c5c3b40c64e55a263387d543d55
KL <dev.lau3@gmail.com>
2020-06-13 15:14:32 -0500

    initial commit for deploy to heroku

---
+++ index.js
@@ -0,0 +1,19 @@
+//Command to start "node index.js"
+//Image files will be server from ./public folder
+
+const express = require('express');
+const app = express();
+const PORT = 8899;
+
+//files will be served from ./public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Connected Express.js server!');
+});
+
+//To connect from client: http://localhost:8899/
+// app.listen(PORT, () => console.log(`Express server listening on port: ${PORT}`));
+
+//To use allocated port on Heroku
+app.listen(process.env.PORT, () => console.log(`Express server listening on port: ${process.env.PORT}`));
